0f5ee1a7b30cacc55be386e93b42967031190529
Pavel <pavgrebens@mail.ru>
2023-01-26 14:21:29 +0300

    Added new request

--- Algorithms/task_5.py
+++ Algorithms/task_5.py
@@ -4,24 +4,32 @@ from pprint import pprint
 
 def use_api():
    name = "apache/spark"
-   url = f"https://api.github.com/repos/{name}"
+   username = name.split("/")[0]
+   get_data_repos = f"https://api.github.com/repos/{name}"
+   get_user_data = f"https://api.github.com/users/{username}"
    
-   response = requests.get(url)
-   if response.status_code == 200:
-      with open("get_json_data.txt", encoding="utf-8", mode="w+") as file:
-           data = response.json()
+   response_repos = requests.get(get_data_repos)
+   response_user = requests.get(get_user_data)
+   if response_repos.status_code == 200 and response_user.status_code == 200:
+      with open("get_json_data.json", encoding="utf-8", mode="w+") as file:
 
+           response_items = {
+               "repos": response_repos.json(),
+               "users": response_user.json(),
+           }
 
            data_items = {
                "data": {
-                  "id": data["id"],
-                  "created_at": data["created_at"],
-                  "name": data["name"],
-                  "url": data["url"],
+                  "company": response_items["users"]["company"],
+                  "email": response_items["users"]["email"],
+                  "id": response_items["repos"]["id"],
+                  "created_at": response_items["repos"]["created_at"],
+                  "name": response_items["repos"]["name"],
+                  "url": response_items["repos"]["url"],
                }
            }
 
-           json.dump(data_items, file)
+           json.dump(data_items, file, indent=4)
 
    else:
       return "Соединение не удалось"
